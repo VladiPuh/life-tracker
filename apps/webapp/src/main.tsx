4383832a8fd1ab483cbacb7d545f4bf52fdb526b
Vladislav <vladislav.tt95@gmail.com>
2026-02-03 01:54:23 +0200

    refactor(webapp): move Telegram init to App bootstrap

--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { hasTelegramWebApp, initTelegram } from "./shared/tg/webapp";
 
-// ВАЖНО: готовим Telegram WebApp ДО первого рендера,
-// чтобы initData был доступен до первых API-запросов.
-if (hasTelegramWebApp()) {
-  initTelegram();
-}
+// Telegram WebApp инициализируется внутри App (bootstrap)
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
