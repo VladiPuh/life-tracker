7d6a92f445de41ffbe0bb1530767237002dbe02b
Vladislav <vladislav.tt95@gmail.com>
2026-02-05 07:37:33 +0200

    fix: remove unused HistoryEditCard import

--- apps/webapp/src/pages/history/HistoryChallengeEdit.tsx
+++ apps/webapp/src/pages/history/HistoryChallengeEdit.tsx
@@ -6,7 +6,6 @@ import { HistoryEditHeader } from "./components/HistoryEditHeader";
 import { HistoryStatusPicker } from "./components/HistoryStatusPicker";
 import { HistoryMinutesInput } from "./components/HistoryMinutesInput";
 import { HistoryCommentInput } from "./components/HistoryCommentInput";
-import { HistoryEditCard } from "./components/HistoryEditCard";
 
 type Draft = {
   status_view: "MIN" | "BONUS" | "SKIP" | "FAIL";
